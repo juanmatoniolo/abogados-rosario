8323cd2c21be980c98899add2f6d1bf1be92a5a0
juanmatoniolo <juanmatoniolo2@gmail.com>
2025-09-08 11:46:28 -0300

    export defaul

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,16 @@
-// tailwind.config.js
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-	content: [
-		"./src/app/**/*.{js,jsx,ts,tsx}",
-		"./src/components/**/*.{js,jsx,ts,tsx}",
-	],
-	theme: {
-		extend: {
-			colors: {
-				primary: "#1e3a8a",
-				secondary: "#2563eb",
-			},
-		},
-	},
-	plugins: [],
+export default {
+  content: [
+    "./src/app/**/*.{js,jsx,ts,tsx}",
+    "./src/components/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#1e3a8a",   // azul oscuro
+        secondary: "#2563eb", // azul más claro
+      },
+    },
+  },
+  plugins: [],
 };
